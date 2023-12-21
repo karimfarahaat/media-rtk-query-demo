ae0ef4ede25198faefaa081786521be0bb555e7f
Karim Farahat <karim.f.farahat@pwc.com>
2023-12-21 17:30:56 +0200

    First Commit

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import {
+  albumsApi,
+  // useFetchAlbumsQuery
+} from "./apis/albumsApi";
+
+export const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    // albums: albumsApi.reducer,
+    //the one below is the same as the one above
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
+  },
+});
+
+setupListeners(store.dispatch);
+
+export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
+export * from "./thunks/removeUser";
+export {
+  useFetchAlbumsQuery,
+  useAddAlbumMutation,
+  useRemoveAlbumMutation,
+} from "./apis/albumsApi";
